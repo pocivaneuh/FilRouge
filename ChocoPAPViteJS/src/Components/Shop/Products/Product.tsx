bb6e21cbd3df446a878d0460e5a9e0a307d179ea
Septenia <159175089+Septenia@users.noreply.github.com>
2024-06-06 09:11:53 +0200

    Update Product.tsx Viiiiiiiiite

--- ChocoPAPViteJS/src/Components/Shop/Products/Product.tsx
+++ ChocoPAPViteJS/src/Components/Shop/Products/Product.tsx
@@ -7,61 +7,63 @@ function handleClick(titleArticle) {
   alert(`Vous voulez acheter 1 ${titleArticle}? Très bon choix 🌱✨`);
 }
 
-export const Product = (  idArticle,
+export function Product (  idArticle,
   urlImg,
   titleArticle,
   ratingArticle,
   priceArticle,
-) =>
-{
-  return (
-    <div
-      id="lstProduitsLG"
-      className="container-fluid"
-      onClick={() => handleClick}
-    >
-      <div className="row m-3">
-        <div className="card m-3">
-          <a routerLink={`/produit/'${idArticle}`}>
-            <img
-              className="card-img-top mt-3"
-              src={urlImg}
-              alt={`photo du ${titleArticle}`}
-              title={`photo du ${titleArticle}`}
-            />
-          </a>
-          <div className="card-body">
-            <span>* Indisponible </span>
-            <h1 className="card-title">{titleArticle}</h1>
-            <p className="card-text tarif">{priceArticle} €</p>
-            <p className="card-text"><Rating Score={ratingArticle}></Rating></p>
-            <form
-              className="mt-4"
-              action=""
-              title="Ajouter l'article au panier"
-            >
-              <div className="group">
-                <label for="nbArticles" className="d-block">
-                  Nombre d'articles
-                </label>
-                <input
-                  type="number"
-                  name="nbArticles"
-                  id="nbArticles"
-                  title="Nombre d'articles à ajouter"
-                  min="0"
-                  max="100"
-                  placeholder="0"
-                  required
+) {
+    {
+      return (
+        <div
+          id="lstProduitsLG"
+          className="container-fluid"
+          onClick={() => handleClick}
+        >
+          <div className="row m-3">
+            <div className="card m-3">
+              <a href={`/produit/'${idArticle}`}>
+                <img
+                  className="card-img-top mt-3"
+                  src={urlImg}
+                  alt={`photo du ${titleArticle}`}
+                  title={`photo du ${titleArticle}`}
                 />
-                <button type="submit" className="ajouter">
-                  Ajouter au panier
-                </button>
+              </a>
+              <div className="card-body">
+                <span>* Indisponible </span>
+                <h1 className="card-title">{titleArticle}</h1>
+                <p className="card-text tarif">{priceArticle} €</p>
+                <p className="card-text"><Rating Score={ratingArticle}></Rating></p>
+                <p className="card-text">{ratingArticle}</p>
+                <form
+                  className="mt-4"
+                  action=""
+                  title="Ajouter l'article au panier"
+                >
+                  <div className="group">
+                    <label for="nbArticles" className="d-block">
+                      Nombre d'articles
+                    </label>
+                    <input
+                      type="number"
+                      name="nbArticles"
+                      id="nbArticles"
+                      title="Nombre d'articles à ajouter"
+                      min="0"
+                      max="100"
+                      placeholder="0"
+                      required
+                    />
+                    <button type="submit" className="ajouter">
+                      Ajouter au panier
+                    </button>
+                  </div>
+                </form>
               </div>
-            </form>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  );
+      );
+    };
 };
