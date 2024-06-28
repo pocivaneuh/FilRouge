c8310ad574600f894cbc0f721a2934e31d137943
Sullivan SENECHAL <soullivaneuh@gmail.com>
2024-06-28 12:36:59 +0200

    feat: working carousel with refactor

--- ChocoPAPViteJS/src/Components/Home/Home.tsx
+++ ChocoPAPViteJS/src/Components/Home/Home.tsx
@@ -1,44 +1,18 @@
 
-import { Carousel } from "./Caroussel/Carousel.tsx";
-import { NavCarousel } from "./Caroussel/NavCarousel/NavCarousel.tsx";
+import { Carousel } from "./Carousel/Carousel.tsx";
 
-import { useState} from "react";
-
-import "../../App.css";
 import "./Home.css";
-
-
-import { slidesList } from '../../Datas/slidesList.ts';
 
 import { Link } from "react-router-dom";
 
 export const Home = ( ) =>
-  {
-    const [isActive, setIsActive] = useState(1);
-    
+{
     return (
     <main id="ContenuPrincipal">
-            <div className="carousel-container row">
-              <div id="crslChocoAccueil" className="carousel slide" data-ride="carousel">
-                 {slidesList.map(({ slideClass, idSlide, urlImgSlide, textAltImgSlide }) => {
-                    if (idSlide === isActive) {
-                      return <Carousel
-                              slideClass = "carousel-item"
-                              idSlide={idSlide}
-                              urlImgSlide={urlImgSlide}
-                              textAltImgSlide={textAltImgSlide}
-                            />;
-                    }
-                    return null;
-                 })}
-              </div>
-              <div className="navigation">
-                  <NavCarousel />
-              </div>
-            </div>
-        <div className="boutique text-style1" >
-          <Link to={`/Boutique`}>VOIR LA BOUTIQUE</Link>
-        </div>
+      <Carousel />
+      <div className="boutique text-style1" >
+        <Link to={`/Boutique`}>VOIR LA BOUTIQUE</Link>
+      </div>
     </main>
   );
 };
