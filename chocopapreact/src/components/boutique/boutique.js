4a29d48512579c4d9c875fc70f57eb92b1484ba1
Sullivan SENECHAL <soullivaneuh@gmail.com>
2024-08-16 13:25:55 +0200

    Mes modifs

--- chocopapreact/src/components/boutique/boutique.js
+++ chocopapreact/src/components/boutique/boutique.js
@@ -2,14 +2,12 @@ import { useState } from 'react';
 import { prdListe } from '../../datas/prdListe';
 import { produit } from './produit';
 
-
 import './boutique.scss';
-
 
 function ShoppingList() {
   return (
     /* CONTENU BOUTIQUE */
-    <section id="ContenuPrincipal">
+    <section id="ContenuPrincipal42">
       <section className="row m-0">
         <div id="selectProduits" className="col-5 col-lg-3"></div>
         <div id="lstProduits" className="col-7 col-lg-9">
